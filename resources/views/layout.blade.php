66b6444d82f1890112326ff451762ddb9c1566b3
Sagar <sagarjha698@gmail.com>
2020-08-01 21:47:37 +0530

    homework till render

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,8 +17,8 @@ Released   : 20140225
     <meta name="keywords" content="" />
     <meta name="description" content="" />
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-    <link href="css/default.css" rel="stylesheet"/>
-    <link href="css/fonts.css" rel="stylesheet"/>
+    <link href="/css/default.css" rel="stylesheet"/>
+    <link href="/css/fonts.css" rel="stylesheet"/>
     <link rel="stylesheet" href="/css/app.css">
 
 
@@ -28,15 +28,15 @@ Released   : 20140225
 <div id="header-wrapper">
     <div id="header" class="container">
         <div id="logo">
-            <h1><a href="#">SimpleWork</a></h1>
+            <h1><a href="/">SimpleWork</a></h1>
         </div>
         <div id="menu">
             <ul>
-                <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
-                <li><a href="#" accesskey="2" title="">Our Clients</a></li>
-                <li><a href="/about" accesskey="3" title="">About Us</a></li>
-                <li><a href="#" accesskey="4" title="">Careers</a></li>
-                <li><a href="#" accesskey="5" title="">Contact Us</a></li>
+                <li class="{{Request::path()==='/'?'current_page_item':''}}"><a href="/" accesskey="1" title="">Homepage</a></li>
+                <li>class= "{{Request::path()==='clients'?'current_page_item':''}}"><a href="#" accesskey="2" title="">Our Clients</a></li>
+                <li>class="{{Request::path()==='about'?'current_page_item':''}}"><a href="/about" accesskey="3" title="">About Us</a></li>
+                <li>class="{{Request::path()==='articles'?'current_page_item':''}}"><a href="/articles" accesskey="4" title="">Articles</a></li>
+                <li>class="{{Request::path()==='contact'?'current_page_item':''}}"><a href="#" accesskey="5" title="">Contact Us</a></li>
             </ul>
         </div>
     </div>
